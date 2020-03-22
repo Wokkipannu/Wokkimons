9c07b3b2ede6a0efcda7cf51eb8b7ecca9fef650
Wokki <iiro@kooditaikuri.fi>
2020-03-22 17:40:27 +0200

    Moved image to imgur

--- monsters/froge.js
+++ monsters/froge.js
@@ -5,8 +5,8 @@ module.exports = class Froge extends Monster {
     super(level);
 
     this.name = 'Pet Froge'
-    this.image = 'https://media.discordapp.net/attachments/381520882608373761/685898770478661686/froggi.gif';
-    this.shinyImage = 'https://media.discordapp.net/attachments/381520882608373761/685898770478661686/froggi.gif';
+    this.image = 'https://i.imgur.com/ZnJ2Aj1.gif';
+    this.shinyImage = 'https://i.imgur.com/ZnJ2Aj1.gif';
     this.rarity = 3;
     this.isShiny = false;
   }
