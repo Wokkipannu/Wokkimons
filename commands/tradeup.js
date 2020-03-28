0e2c4af8a239899c97653e7e14ec294779c639e3
Wokki <iiro@kooditaikuri.fi>
2020-03-29 01:08:27 +0200

    Added a tradeup command
    Trade up allows exchanting 10 of the same type monster into a higher rarity 1

---
+++ commands/tradeup.js
@@ -0,0 +1,64 @@
+/**
+ * Command Tradeup
+ * 
+ * Trade several lower tier monsters to get a random higher tier monster
+ * Has a possibility of giving a shiny alternative of the monster
+ */
+
+const { MessageEmbed } = require('discord.js');
+const Monsters = require('../utils/monsters');
+const PlayerController = require('../controllers/PlayerController');
+const MonController = require('../controllers/MonController');
+const MonsterController = require('../controllers/MonsterController');
+
+module.exports = {
+  name: 'tradeup',
+  description: 'Trade 10 lower tier monsters to a higher tier one',
+  guildOnly: false,
+  async execute(msg, args) {
+    // Fetch the player from database
+    // If we don't have a player in the database, we'll send them a message
+    let player = await PlayerController.getPlayer(msg.author.id);
+    if (!player) return msg.reply('Sinulla ei ole yhtään monsteria!');
+    // Get the player embed color or use default if not defined
+    let color = player.color || '#1e90ff';
+    // Validate the args
+    if (args.length < 10 || args.length > 10) return msg.reply('Sinun tulee antaa tasan 10 ID:tä');
+    let monsters = [];
+    let previous;
+    args.forEach(arg => {
+      arg = parseInt(arg);
+      let mon = player.monsters.find(m => m.id === arg);
+      if (mon) {
+        if (!previous) previous = mon;
+        if (previous && previous.Mon.name !== mon.Mon.name) return;
+        else monsters.push(mon);
+      }
+    });
+    if (monsters.length !== 10) return msg.reply('Varmista, että kaikki antamasi monsterit ovat samoja monstereita');
+    // Create the new monster
+    let mons = await MonController.getAllMons();
+    mons = mons.filter(m => m.rarity === monsters[0].Mon.rarity + 1);
+    let mon = mons[Math.floor(Math.random() * mons.length)];
+    mon.level = Monsters.getLevel();
+    mon.isShiny = this.rollShiny();
+    // Remove monsters from player
+    monsters.forEach(async mon => await MonsterController.deleteMonster(mon.id));
+    // Give the new monster to the player
+    await MonsterController.createMonster({ MonId: mon.id, level: mon.level, isShiny: mon.isShiny, PlayerId: player.id });
+    // Send monster exchange embed
+    const embed = new MessageEmbed()
+      .setColor(color)
+      .setTitle(`${msg.author.username} » Tradeup`)
+      .addField('Traded monsters', `${monsters.map(mo => `Tason ${mo.level} ${mo.isShiny ? `⭐ ${mo.Mon.name}` : mo.Mon.name}`).join('\n')}`, true)
+      .addField('Received monster', `Tason ${mon.level} ${mon.isShiny ? `⭐ ${mon.name}` : mon.name}`, true);
+
+    msg.channel.send(embed);
+  },
+
+  rollShiny() {
+    let die = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    if (die === 50) return 1;
+    else return 0;
+  }
+}
