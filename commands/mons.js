9f67e6e4fdba06e4aa81b604b45f92bb1bd5f32c
Wokki <iiro@kooditaikuri.fi>
2020-03-30 03:45:20 +0300

    Update mons.js

--- commands/mons.js
+++ commands/mons.js
@@ -29,6 +29,7 @@ module.exports = {
     const uncommon = mons.filter(monster => monster.Mon.rarity === 2);
     const rare = mons.filter(monster => monster.Mon.rarity === 3);
     // Get all monsters and sort them by rarities
+    // Then sort each rarity group alphabetically
     const Mons = await MonController.getAllMons();
     const commonMonsters = Mons.filter(m => m.rarity === 1).sort((a, b) => {
       let textA = a.name.toUpperCase();
@@ -45,8 +46,7 @@ module.exports = {
       let textB = b.name.toUpperCase();
       return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
     });
-
-
+    // Create and send embed
     let embed = new MessageEmbed()
       .setColor(color)
       .setTitle(`${msg.author.username} » Collection`);
